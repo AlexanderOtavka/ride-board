a27f8b3e9749e032a8f6056943564b405de522b1
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2019-11-18 17:44:17 -0600

    Tweak routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :locations
-  devise_for :users
   root to: "rides#index"
 
   resources :rides
+  resources :locations
+  devise_for :users, path: "me"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
