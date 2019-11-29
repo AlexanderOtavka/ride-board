05325f43caca4059c211bfb79d289d92391ec8ab
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2019-11-29 12:42:41 -0600

    Fix welcome tests

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -3,20 +3,8 @@ require 'test_helper'
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
-  test "should get index when no user is signed in" do
+  test "should get index" do
     get root_url
     assert_response :success
-  end
-
-  test "should redirect to passenger app when user is a passenger" do
-    sign_in users(:passenger)
-    get root_url
-    assert_redirected_to passenger_root_url
-  end
-
-  test "should redirect to driver app when user is a driver" do
-    sign_in users(:driver)
-    get root_url
-    assert_redirected_to driver_root_url
   end
 end
