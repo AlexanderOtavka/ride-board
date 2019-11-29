4b00dc725b86549ffe054c0afc4706c034f02097
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2019-11-29 12:39:32 -0600

    Remove drives field from user

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,4 @@
 class WelcomeController < ApplicationController
   def index
-    if user_signed_in?
-      if current_user.drives?
-        redirect_to driver_root_path
-      else
-        redirect_to passenger_root_path
-      end
-    end
   end
 end
--- db/migrate/20191119203245_add_drives_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddDrivesToUser < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :drives, :boolean
-  end
-end
