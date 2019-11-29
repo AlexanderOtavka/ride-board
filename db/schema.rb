9e0e7e618f8bfb273fbf01ffc898f46a848997df
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2019-11-29 12:39:32 -0600

    Remove drives field from user

--- db/schema.rb
+++ db/schema.rb
@@ -64,7 +64,6 @@ ActiveRecord::Schema.define(version: 2019_11_23_004951) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin"
-    t.boolean "drives"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
