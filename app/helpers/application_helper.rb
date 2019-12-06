8714adafb667d2b87435d73de8b71a40372fad97
yashdavisgupta <yashdavisgupta@gmail.com>
2019-12-06 13:32:53 -0600

    Fix asset inclusion

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
     match = user.email.match(/\A(.*)@/)
     "[#{match.captures[0]}]"
   end
+  def controller_stylesheet(opts = { media: :all })
+    if Rails.application.assets.find_asset("#{params[:controller]}.css")
+      stylesheet_link_tag(params[:controller], opts)
+    end
+  end
+  def controller_javascript(opts = {})
+    if Rails.application.assets.find_asset("#{params[:controller]}.js")
+      javascript_include_tag(params[:controller], opts)
+    end
+  end
 end
