cb358befb138c07e7c25a0ed79a008dc844f0672
yashdavisgupta <yashdavisgupta@gmail.com>
2019-12-08 21:40:39 -0600

    Fix Login and add toast error display

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_12_04_050017) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "locations", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
