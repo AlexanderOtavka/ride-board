89520decf05319c438b6641e1c3ff1b00188766e
yashdavisgupta <yashdavisgupta@gmail.com>
2019-12-09 17:51:37 -0600

    Streamline form initialization with AutoInit()

--- app/assets/javascripts/init-elements.js
+++ app/assets/javascripts/init-elements.js
@@ -1,8 +1,3 @@
-document.addEventListener("turbolinks:load", function() {
-  var elems = document.querySelectorAll("select");
-  var instances = M.FormSelect.init(elems);
-  var elems = document.querySelectorAll('.datepicker');
-  var instances = M.Datepicker.init(elems);
-  var elems = document.querySelectorAll('.timepicker');
-  var instances = M.Timepicker.init(elems);
+document.addEventListener('DOMContentLoaded', function() {
+  M.AutoInit();
 });
