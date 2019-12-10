9017b99e6e8ea97e570acd746f4f2386f4bed2a3
root <root@2ae223741ec8>
2019-12-10 14:26:47 +0000

    Add mailgun initializer + init for mail service

--- app/services/notifier/email_notifier.rb
+++ app/services/notifier/email_notifier.rb
@@ -1,8 +1,26 @@
+require 'mailgun-ruby'
+
 module Notifier
   class EmailNotifier < NotifierBase
+    def initializer(real_messages)
+      super(real_messages)
+      if real_messages
+        @mg_client = Mailgun::Client.new 'api-key'
+      end
+      @logger = Rails.logger
+    end
     # user: a user model
     # message: string
     def send_notification(user, message)
+
+    end
+    def _build_message(from_email, to_email, email_subject, body)
+      return {
+          from: from_email,
+          to: to_email,
+          subject: email_subject,
+          text: body
+      }
     end
   end
 end
