604c4c42daa288e95cc0552083da1f4dddce514b
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2019-12-10 15:47:41 -0600

    Add my rides for drivers

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,31 @@
 Rails.application.routes.draw do
   root to: "welcome#index"
 
+  devise_for :users
+  resources :locations
+
+  get "/s/:id", to: redirect('/passenger/rides/%{id}'), as: :share_ride
+
   namespace :passenger do
     root to: "rides#index"
 
+    post   "/rides/:id/join", to: "rides#join",  as: :join_ride
+    delete "/rides/:id/join", to: "rides#leave"
     resources :rides do
       resources :messages, as: :messages, only: [:create]
     end
-    post   "/rides/:id/join", to: "rides#join",  as: :join_ride
-    delete "/rides/:id/join", to: "rides#leave"
   end
-
-  get "/s/:id", to: redirect('/passenger/rides/%{id}'), as: :share_ride
 
   namespace :driver do
     root to: "rides#index"
 
+    get    "/rides/mine",     to: "rides#mine",  as: :my_rides
+    post   "/rides/:id/join", to: "rides#join",  as: :join_ride
+    delete "/rides/:id/join", to: "rides#leave"
     resources :rides do
       resources :messages, as: :messages, only: [:create]
     end
-    post   "/rides/:id/join", to: "rides#join",  as: :join_ride
-    delete "/rides/:id/join", to: "rides#leave"
   end
-
-  devise_for :users
-  resources :locations
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
