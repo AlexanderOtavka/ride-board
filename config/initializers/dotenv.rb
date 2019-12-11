56d23f1770fcc917e0f349ea37959703eba2fb11
Tran Le <lequytra@grinnell.edu>
2019-12-11 02:33:50 +0000

    Add required keys

--- config/initializers/dotenv.rb
+++ config/initializers/dotenv.rb
@@ -2,5 +2,8 @@ Dotenv.require_keys('SEND_REAL_MESSAGES')
 ENV['SEND_REAL_MESSAGES'] = ENV['SEND_REAL_MESSAGES'].downcase
 
 if ENV['SEND_REAL_MESSAGES'] == "true"
-  Dotenv.require_keys('AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_REGION')
+  Dotenv.require_keys('AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_REGION',
+                      'MAILGUN_API_KEY', 'MAILGUN_DOMAIN')
 end
+
+
