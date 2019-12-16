08326f3f29aa77fd93d004eb69048d67311d68da
Tran <lequytra@grinnell.edu>
2019-12-16 17:40:29 -0600

    Add email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
 
   has_many :seat_assignments, dependent: :destroy
@@ -28,4 +28,10 @@ class User < ApplicationRecord
       "(#{area_code}) #{prefix}-#{suffix}"
     end
   end
+
+  protected
+  # Uncomment this if we want to skip email confirmation
+  #def confirmation_required?
+  #  false
+  #end
 end
