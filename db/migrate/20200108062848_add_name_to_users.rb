097f2c3044c72df0bc058772e76a85a0566ce43f
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2020-01-08 01:53:24 -0800

    Fix heroku build issue

--- db/migrate/20200108062848_add_name_to_users.rb
+++ db/migrate/20200108062848_add_name_to_users.rb
@@ -1,4 +1,4 @@
-class AddDisplayNameToUsers < ActiveRecord::Migration[6.0]
+class AddNameToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :name, :string, null: false, default: ""
   end
