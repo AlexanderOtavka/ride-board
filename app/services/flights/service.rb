8852e441d7dad140e902c7ef55906d2ed6dcfb66
YourFin <d.junkpobox+git@gmail.com>
2020-01-09 13:41:36 -0800

    Add working flightaware client, combine find_flight methods

--- app/services/flights/service.rb
+++ app/services/flights/service.rb
@@ -1,23 +1,39 @@
-class Flights::Service
-  # Takes date and flight number and returns precise flight times,
-  # and hopefully a flightaware faFlightID we can use to keep track of the flight
-  # in the future
-  #
-  # Possible results: ApiError, FlightNotFound, NonEmpty [Flight]
-  #
-  # Will stem mostly from https://flightaware.com/commercial/flightxml/explorer/#op_AirlineFlightSchedules
-  #
-  # Caller note: there do not appear to be guarantees that there will only be one
-  # flight for a given flight number, so we should be prepared to handle the case
-  # where we get multiple back
-  def find_flight_by_number
+require 'savon'
+
+module Flights
+  SOAP_URL = 'http://flightxml.flightaware.com/soap/FlightXML2/wsdl'
+
+
+  class Service
+    def initialize
+      if ENV['USE_REAL_FLIGHTAWARE_API'] == "true"
+        @client = Savon.client(
+          wsdl: SOAP_URL,
+          basic_auth: [ENV['FLIGHTAWARE_API_USERNAME'],
+                       ENV['FLIGHTAWARE_API_KEY']]
+        )
+      end
+    end
+
+    # Takes date, and either a flight number (with carrier) or airports,
+    # and returns precise flight times, and hopefully a flightaware faFlightID
+    # we can use to keep track of the flight in the future
+    #
+    # Possible return values: ApiError, [Flight]
+    #
+    # Will stem mostly from https://flightaware.com/commercial/flightxml/explorer/#op_AirlineFlightSchedules
+    #
+    # Caller note: there do not appear to be guarantees that there will only be one
+    # flight for a given flight number, so we should be prepared to handle the case
+    # where we get multiple back
+    def find_flight(date:,
+                    airline: nil,
+                    flight_number: nil,
+                    departure_airport: nil,
+                    destination_airport: nil)
+    end
+
+    # Errors to handle: SocketError -> Failed to open TCP connection due to name resolution failure
   end
 
-  # Similar to find_flight_by_number, but returns a list of flights to pick from
-  #
-  # Possible results: ApiError, [Flight]
-  #
-  # Uses same api endpoint as find_flight_by_number
-  def find_flight_by_airport
-  end
 end
