3319c99df19ae97510eae9aba931f61b62fe03f5
Tran <lequytra@grinnell.edu>
2020-01-12 15:57:18 +0700

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -82,6 +82,11 @@ ActiveRecord::Schema.define(version: 2019_12_16_231717) do
     t.boolean "notify_email"
     t.string "phone_number"
     t.string "name", default: "", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
 
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
