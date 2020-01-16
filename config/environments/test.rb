edba7ced311a0fe6455cbf15f45b48d2cd1d3d21
Tran <lequytra@grinnell.edu>
2020-01-16 14:17:02 +0700

    Test changing test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,7 +38,17 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
+  #config.action_mailer.delivery_method = :test
+  # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+      api_key: ENV['MAILGUN_API_KEY'],
+      domain: ENV['MAILGUN_DOMAIN'],
+      # api_host: 'api.eu.mailgun.net'  # Uncomment this line for EU region domains
+  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
