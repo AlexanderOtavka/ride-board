4a010931c4e621b0e08ffd6d3b9fbfb9e78c8f0e
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2020-01-17 12:12:04 -0800

    Fix missing ahoy.js bug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import ahoy from "ahoy.js";
+// import ahoy from "ahoy.js";
 
 require("@rails/ujs").start()
 require("turbolinks").start()
